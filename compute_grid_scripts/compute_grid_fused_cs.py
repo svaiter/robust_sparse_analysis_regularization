e93cd0f71993f53d03e96d8641a3e268000ac12b
Samuel Vaiter <samuel.vaiter@gmail.com>
2012-09-28 11:48:34 +0200

    Revised version

--- compute_grid_scripts/compute_grid_fused_cs.py
+++ compute_grid_scripts/compute_grid_fused_cs.py
@@ -25,7 +25,7 @@ def grid_ratio_eta(eps_candidates, ratio_candidates, eta_candidates, trials):
     import itertools
     import multiprocessing as mp
     pos = itertools.product(eps_candidates, ratio_candidates, eta_candidates, range(trials))
-    pool = mp.Pool()
+    pool = mp.Pool(processes=30)
     res_tmp = pool.map_async(dummy, pos)
     return res_tmp
 
@@ -34,6 +34,6 @@ eta_candidates = np.arange(1,np.floor(0.2*n)) / n
 ratio_candidates = np.arange(round(n/2),n+1,2) / n
 eps_candidates = [50/n,]
 
-restmp=grid_ratio_eta(n, eps_candidates, ratio_candidates, eta_candidates, trials_per_settings).get()
+restmp=grid_ratio_eta(eps_candidates, ratio_candidates, eta_candidates, trials_per_settings).get()
 restarr = np.asarray(restmp)
-np.save('fused_final_full',restarr)
+np.save('fused_final_ratio_eta',restarr)
